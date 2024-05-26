456db0c3e245aedfebea555680e2f112ee9b2ae7
cammieleighp <cammie.peterson18@gmail.com>
2024-05-25 18:03:31 -0600

    rest file

--- Controllers/genres.js
+++ Controllers/genres.js
@@ -12,19 +12,42 @@ const getAll = async (req, res, next) => {
   };
 
   const createGenre = async (req, res, next) => {
-    const contact = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      birthdate: req.body.birthdate,
-      faveColor: req.body.faveColor,
+    const genre = {
+      name: req.body.name,
+      ages: req.body.ages,
+      aisle: req.body.aisle,
     }
-    const response = await mongodb.getDb().db().collection('genres').insertOne(contact);
+
+    const response = await mongodb.getDb().db().collection('genres').insertOne(genre);
     if (response.acknowledged) {
       res.status(201).json(response);
     } else {
       res.status(500).json(response.error || 'Some error occurred while creating the contact.');
     }
+  }
+
+  const getSingle = async (req, res, next) => {
+    const result = await mongodb.getDb().db().collection('genres').find();
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
+  }
+
+  const updateGenre = async (req, res, next) => {
+    const result = await mongodb.getDb().db().collection('genres').find();
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
+  }
+
+  const deleteGenre = async (req, res, next) => {
+    const result = await mongodb.getDb().db().collection('genres').find();
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
   }
 
   module.exports = { getAll, createGenre};
